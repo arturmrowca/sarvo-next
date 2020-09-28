813d7326bcf8f9d6b3093c4ee14ca8adc348f808
Artur <Z>
2020-09-28 21:53:05 +0200

    a

--- backend/project/database.py
+++ backend/project/database.py
@@ -28,35 +28,3 @@ def config():
         'HOST': "ls-4bd4b70dde975f755fa784f44fb7c6defa761dc9.cp2sma0o3rti.eu-central-1.rds.amazonaws.com",
         'PORT': "5432"
     }
-    """return {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "sarvodb",
-        'USER': "sarvo",
-        'PASSWORD': "YufBBhxTY4AA12FFFfpuS5",
-        'HOST': "sarvo.io",
-        'PORT': "5432"
-    }"""
-
-
-"""
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-def config():
-    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    if service_name:
-        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
-    else:
-        engine = engines['sqlite']
-    name = os.getenv('DATABASE_NAME')
-    if not name and engine == engines['sqlite']:
-        name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
-    return {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': "sarvodb",
-        #'USER': "sarvo",
-        #'PASSWORD': "YufBBhxTY4AA12FFFfpuS5",
-        #'HOST': "sarvo.io",
-        #'PORT': "5432"
-    }
-"""
