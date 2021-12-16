7b0ae58283fec8412167c1f01e54d968b7bcdf97
Artur <Z>
2021-12-16 17:04:44 +0100

    m

---
+++ backend/project/wsgi.py
@@ -0,0 +1,19 @@
+import os
+
+import os
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+
+
+import sys
+sys.path.append('/home/bitnami/travweb/traveweb/django/src')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/home/bitnami/travweb/traveweb/django/src/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+os.environ["HTTPS"] = "on"
+os.environ["wsgi.url_scheme"] = "https"
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
